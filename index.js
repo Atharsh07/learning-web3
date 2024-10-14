a75433af73edba2eb471c367693ee42c0d17a106
atharsh07 <atharsh0425@gmail.com>
2024-10-14 23:45:37 +0530

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const crypto = require('crypto')
-const input = 'atharsh';
-const hash = crypto.createHash('sha256').update(input).digest('hex');
+// the assigement is that the hashed value of an string should start with 00000 , have to find out the string and full
+// hashed hash
 
-console.log(hash);
+const crypto = require('crypto')
+
+function checkthezero(prefix) {
+    let input = 0
+    while (true) {
+        let inputstr = "100xdevs" + input.toString()
+        let hash = crypto.createHash('sha256').update(inputstr).digest('hex')
+        if(hash.startsWith(prefix)){
+            return {input:inputstr, hash:hash}
+        }
+        input++
+    }
+}
+const result = checkthezero('00000')
+console.log(`Input: ${result.input}`);
+console.log(`Input: ${result.hash}`);
